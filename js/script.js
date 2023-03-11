11901082e70c0863eb47c5ef6ba4ea2d8157a06f
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-03-11 01:50:12 -0300

    faq/footer

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ function modal() {
 }
 //modal();
 
+function faq() {
+
+    const quest = document.querySelectorAll(".quest");
+    const aswer = document.querySelectorAll(".aswer");
+    const img_icon = document.querySelectorAll('.quest img');
+
+    for (let i = 0; i < quest.length; i++) {
+       
+        quest[i].addEventListener("click", () => {
+
+            if( quest[i].classList.contains("close") ) {
+                quest[i].classList.toggle("close");
+                aswer[i].classList.toggle("open");
+                img_icon[i].src ='img/+.png';
+            }else {
+                quest[i].classList.add("close");
+                aswer[i].classList.add("open");
+               
+                img_icon[i].src ='img/-.png';
+            }
+
+
+        });
+
+    }
+
+
+}
+faq();
