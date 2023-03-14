8561edcecfc93c6b7c0841c64d10dff0c6955596
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-03-14 15:48:37 -0300

    home change imgs/modal js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-function menu(){
+function menu() {
     $(".menu_mobile_open").click(
-        function(){
-            
-            $('.menu_mobile').css( 'display', 'flex' ).css( 'top', '80px' ).css( 'animation', 'move ease-in 400ms' );
-            setTimeout( ()=> {
-                $('.menu_mobile_open').css( 'display', 'none' );
-                $('.menu_mobile_close').css( 'display', 'flex' );
-            }, 200 );
-        }  
+        function () {
+
+            $('.menu_mobile').css('display', 'flex').css('top', '80px').css('animation', 'move ease-in 400ms');
+            setTimeout(() => {
+                $('.menu_mobile_open').css('display', 'none');
+                $('.menu_mobile_close').css('display', 'flex');
+            }, 200);
+        }
     );
 
     $(".menu_mobile_close").click(
-        function() {
-            $('.menu_mobile').css( 'top', '-600%' ).css('animation', 'move ease-out 400ms;' );
-            setTimeout( () => {
-                if($('.menu_mobile').css('top', '-600%')) {
+        function () {
+            $('.menu_mobile').css('top', '-600%').css('animation', 'move ease-out 400ms;');
+            setTimeout(() => {
+                if ($('.menu_mobile').css('top', '-600%')) {
                     $('.menu_mobile').css('display', 'none');
                 }
             }, 600);
 
-            setTimeout( ()=> {
-                $('.menu_mobile_open').css( 'display', 'flex' );
-                $('.menu_mobile_close').css( 'display', 'none' );
-            }, 100 );
-            
+            setTimeout(() => {
+                $('.menu_mobile_open').css('display', 'flex');
+                $('.menu_mobile_close').css('display', 'none');
+            }, 100);
+
         }
-        
+
     );
 }
 menu();
@@ -33,20 +33,34 @@ menu();
 function modal() {
 
     let galery = document.querySelectorAll('.area_galery img');
-
+    let close = document.querySelector('.area_close img');
+    document.querySelector('.modal_galery').style.animation = '';
     galery.forEach((element) => {
 
-        element.addEventListener('click', () => {
 
-            document.querySelector('.area_modal_galery img').src = element.getAttribute('src');
-            document.querySelector('.modal_galery').style.display = 'flex';
-            //console.log(element.getAttribute('src'));
-        });
+        setTimeout(() => {
+            element.addEventListener('click', () => {
+
+                document.querySelector('.modal_img').src = element.getAttribute('src');
+                document.querySelector('.modal_galery').style.animation = 'modal ease 400ms';
+                document.querySelector('.modal_galery').style.display = 'flex';
+
+            });
+
+        }, 100);
 
     });
 
+    close.addEventListener('click', () => {
+        document.querySelector('.modal_galery').style.animation = 'modal_out ease-out 400ms';
+
+        setTimeout(() => {
+            document.querySelector('.modal_galery').style.display = 'none';
+        }, 200);
+    });
+
 }
-//modal();
+modal();
 
 function faq() {
 
@@ -55,18 +69,18 @@ function faq() {
     const img_icon = document.querySelectorAll('.quest img');
 
     for (let i = 0; i < quest.length; i++) {
-       
+
         quest[i].addEventListener("click", () => {
 
-            if( quest[i].classList.contains("close") ) {
+            if (quest[i].classList.contains("close")) {
                 quest[i].classList.toggle("close");
                 aswer[i].classList.toggle("open");
-                img_icon[i].src ='img/+.png';
-            }else {
+                img_icon[i].src = 'img/+.png';
+            } else {
                 quest[i].classList.add("close");
                 aswer[i].classList.add("open");
-               
-                img_icon[i].src ='img/-.png';
+
+                img_icon[i].src = 'img/-.png';
             }
 
 
